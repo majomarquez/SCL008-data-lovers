5b5ca29ffda0572a0171f94a9d042e8dfab4c6cf
majomarquez <majomarquez@gmail.com>
2019-02-27 23:31:00 -0300

    funcion para los campeones y para ver las imagenes grandes sn ccs

--- src/main.js
+++ src/main.js
@@ -1,1 +1,47 @@
 /* Manejo del DOM */
+
+
+// function for see all champions just small pictures :)
+var images = ["Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Ashe","Aurelion Sol","Azir","Bard","Blitzcrank",
+"Brand","Braum","Caitlyn","Camille","Cassiopeia","Cho'Gath","Corki","Darius","Diana","Draven","DrMundo","Ekko",
+"Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Hecarim",
+"Heimerdinger","Illaoi","Irelia","Ivern","Janna","Jarvan IV","Jax","Jayce","Jhin","Jinx","Kalista","Karma","Karthus",
+"Kassadin","Katarina","Kayle","Kennen","KhaZix","Kindred","Kled","KogMaw","LeBlanc","Lee Sin","Leona","Lissandra",
+"Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","MasterYi","MissFortune","Wukong","Mordekaiser","Morgana","Nami",
+"Nasus","Nautilus","Nidalee","Nocturne","Nunu","Olaf","Orianna","Pantheon","Poppy","Quinn","Rammus","Rek'Sai","Renekton",
+"Rengar","Riven","Rumble","Ryze","Sejuani","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka",
+"Swain","Syndra","TahmKench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","TwistedFate",
+"Twitch","Udyr","Urgot","Varus","Vayne","Veigar","VelKoz","Vi","Viktor","Vladimir","Volibear","Warwick","Xerath",
+"XinZhao","Yasuo","Yorick","Zac","Zed","Ziggs","Zilean"];
+
+    var row = document.getElementsByClassName("champions");
+	var html_to_insert = '';
+	
+	for (var i = 0; i < images.length-1; i++) {
+		html_to_insert += '<div class="champion-wrapper"><a href="javascript:void(0)" onclick="champDetails(\''+images[i]+'\')">' +
+		'<img src="https://www.masterypoints.com/assets/img/lol/champion_icons/'+images[i]+'.png" width="40" height="40"></a>'+
+		'</div>';
+	}
+	
+	row[0].insertAdjacentHTML('beforeend', html_to_insert);
+	
+	function champDetails(champName) {
+		if (champName != '') {
+			var details = document.getElementsByClassName("champ-details");
+			var champDesc = '<span class="champ-name">' + champName + '</span>'+
+				'<img src="'+LOL.data[champName].splash+'" alt="Big Picture" class="imagen" />'+
+				'<p class="descripcion">'+LOL.data[champName].blurb+'</p>'+
+				'<a href="javascript:void(0)" onclick="backToChampions()" class="boton_de_volver">Back to champ list</a>';
+			details[0].innerHTML = champDesc;
+			var champs = document.getElementsByClassName("champions");
+			champs[0].style.display = 'none';
+		}
+	}
+	
+	function backToChampions() {
+		var champs = document.getElementsByClassName("champions");
+		champs[0].style.display = 'block';
+		var champDet = document.getElementsByClassName("champ-details");
+		
+		champDet[0].innerHTML = '';
+	}
